5de711c1fd578180a9a28bbcc373f36ada00c45b
01fe22bca048 <01fe22bca048@kletech.ac.in>
2024-05-13 17:11:27 +0530

    This is room details

---
+++ roomdetails.java
@@ -0,0 +1,22 @@
+package hotel_booking.new_hotel_booking;
+
+public class roomdetails {
+    int room_no;
+    public void print()
+    {
+       if(room_no==101)
+       {
+        System.out.println("Your room is in first floor");
+       }
+
+       else if(room_no==102)
+       {
+        System.out.println("Your room is in second floor");
+       }
+
+       else
+       {
+        System.out.println("Your room is in Third floor");
+       }
+    }
+}
